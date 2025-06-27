887025efdec9dae2e3a32cfa4c49e523128636b5
AgungNaya <geknaya54@gmail.com>
2025-06-27 22:37:22 +0800

    UI Automation Testing on SauceDemo

---
+++ tests/test-sort.js
@@ -0,0 +1,56 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const assert = require('assert');
+const { Select } = require('selenium-webdriver/lib/select'); 
+const fs = require('fs');
+
+describe('SauceDemo Test - Login and Sort A-Z', function () {
+    this.timeout(30000); 
+
+    let driver;
+
+    before(async () => {
+        driver = await new Builder().forBrowser('firefox').build();
+    });
+
+    after(async () => {
+        await driver.quit();
+    });
+
+    it('should login successfully and sort products A-Z', async () => {
+        await driver.get('https://www.saucedemo.com');
+
+        await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+        await driver.findElement(By.id('password')).sendKeys('secret_sauce');
+        await driver.findElement(By.id('login-button')).click();
+
+        await driver.wait(until.urlContains('inventory.html'), 10000);
+        await driver.wait(until.elementLocated(By.className('inventory_list')), 10000);
+
+        driver.takeScreenshot().then(function(data){
+            fs.writeFileSync('screenshot.png', data, 'base64');
+        });
+
+        const currentUrl = await driver.getCurrentUrl();
+        assert.ok(currentUrl.includes('inventory.html'), 'Login failed and the User was not redirected to inventory page');
+
+        const sortDropdown = await driver.findElement(By.css('[data-test="product-sort-container"]'));
+        assert.strictEqual(await sortDropdown.isDisplayed(), true, 'The Sorting Dropdown is not visible');
+        const select = new Select(sortDropdown); 
+        await select.selectByVisibleText('Name (A to Z)');
+
+        await driver.wait(async () => {
+            const firstItem = await driver.findElement(By.css('.inventory_item_name')).getText();
+            return firstItem === 'Sauce Labs Backpack';
+        }, 5000);
+
+        const nameElements = await driver.findElements(By.className('inventory_item_name'));
+        const productNames = [];
+
+        for (let el of nameElements) {
+            productNames.push(await el.getText());
+        }
+        const sortedNames = [...productNames].sort();
+        assert.deepStrictEqual(productNames, sortedNames, 'Products are not sorted from A to Z');
+    });
+});
